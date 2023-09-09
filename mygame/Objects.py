897808427acc01cd2b5ddf7800bccd70ee7afae9
lim <limjung99@naver.com>
2023-09-10 01:05:40 +0900

    에셋 정리

--- mygame/Objects.py
+++ mygame/Objects.py
@@ -3,19 +3,19 @@ import pygame
 class Assets:
     def __init__(self):
         #image_assets and resizing
-        start_image = pygame.image.load(r"C:\Users\limju\OneDrive\바탕 화면\codes\mygame\assets\first_asset.png")
+        start_image = pygame.image.load("./assets/first_asset.png")
         start_image = pygame.transform.scale(start_image,(750,450))
-        space_invaders = pygame.image.load(r"C:\Users\limju\OneDrive\바탕 화면\codes\mygame\assets\space_invaders.png")
+        space_invaders = pygame.image.load("./assets/space_invaders.png")
         space_invaders = pygame.transform.scale(space_invaders,(300,200))
-        alien_image = pygame.image.load(r"C:\Users\limju\OneDrive\바탕 화면\codes\mygame\assets\alien.png")
+        alien_image = pygame.image.load("./assets/alien.png")
         alien_image = pygame.transform.scale(alien_image,(40,40))
-        boss_image = pygame.image.load(r"C:\Users\limju\OneDrive\바탕 화면\codes\mygame\assets\boss.png")
+        boss_image = pygame.image.load("./assets/boss.png")
         boss_image = pygame.transform.scale(boss_image,(100,100))
-        shuttle_image = pygame.image.load(r"C:\Users\limju\OneDrive\바탕 화면\codes\mygame\assets\shuttle.png")
+        shuttle_image = pygame.image.load("./assets/shuttle.png")
         shuttle_image = pygame.transform.scale(shuttle_image,(40,40))
-        space = pygame.image.load(r"C:\Users\limju\OneDrive\바탕 화면\codes\mygame\assets\space.png")
+        space = pygame.image.load("./assets/space.png")
         space = pygame.transform.scale(space,(750,450))
-        bullet_image = pygame.image.load(r"C:\Users\limju\OneDrive\바탕 화면\codes\mygame\assets\bullet.png")
+        bullet_image = pygame.image.load("./assets/bullet.png")
         bullet_image = pygame.transform.scale(bullet_image,(10,20))
         self.images = {
             "start_image" : start_image,
